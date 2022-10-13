c64882d246a5a9b0de33b02eb9d963602be752be
Bhumika <bhumakhats1994@gmail.com>
2022-10-13 12:49:31 +0530

    Remove the now ignored directory framework

--- framework/container/base.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Framework\Container;
-
-class Base extends \Framework\Base {
-
-}
--- framework/core/exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Framework\Core
-{
-    class Exception extends \Exception
-    {
-        
-    }
-}
